5635de0d3d9a5a7f2e8114d2825846fa84577fbf
tejasyd1094-stack <tejas.yd1094@gmail.com>
2025-12-11 18:15:43 +0530

    Delete lib/currency.ts

--- lib/currency.ts
+++
@@ -1,7 +0,0 @@
-export function formatCurrency(value: number) {
-  return new Intl.NumberFormat("en-IN", {
-    style: "currency",
-    currency: "INR",
-    maximumFractionDigits: 0,
-  }).format(value);
-}
