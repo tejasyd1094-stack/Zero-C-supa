3a27496f351e825df0f881a022906d122fc52ca1
tejasyd1094-stack <tejas.yd1094@gmail.com>
2025-12-15 01:47:52 +0530

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,7 +1,18 @@
-import { createClient } from "@supabase/supabase-js";
+import { createServerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export const supabaseServer = () =>
-  createClient(
+export function supabaseServer() {
+  const cookieStore = cookies();
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
   );
+}
